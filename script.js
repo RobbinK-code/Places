138baadb0a4a0a4178ca06f9e3b0a75b22eaee68
Robbin <robbin.kimani@student.moringaschool.com>
2026-02-10 05:53:36 +0300

    I have made some edits to the index.html file. I added a new div with the id "result" to display the selected place information and another button to show available information about the place. I also included a new div with the id "availableInfo" to display that information. Additionally, I linked the script.js file at the end of the body to ensure that the JavaScript functions work properly.

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ function showPlace() {
     const containerDiv = document.getElementById('container');
     resultDiv.innerHTML = `You selected: ${place}`;
     containerDiv.innerHTML = `<img src="${place.toLowerCase().replace(/ /g, '_')}.jpg" alt="${place}" class="place-image">`;
-    // When Mombasa is selected, displays "mombasa.jpg"
+    // When Mombasa is selected, displays {
+    //When show available info button is clicked, displays(
+    // "Mombasa is a coastal city in Kenya known for its beautiful beaches and rich history."
+    // "img src="https://plus.unsplash.com/premium_photo-1697729911993-626a3e2c44eb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bW9tYmFzYXxlbnwwfHwwfHx8MA%3D%3D" alt="Mombasa" class="place-image">
+
+
+
+
+}
     // When Nairobi is selected, displays "nairobi.jpg"
     // When Mt Longonot is selected, displays "mt_longonot.jpg"
-}
+
 
 
 
